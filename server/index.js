15edb6430cbe1b15ddcc59f4f6c96bd73d526d64
Anisshaikh25 <anisshaikh5691@gmail.com>
2026-09-15 01:02:44 +0530

    Prepare backend for deployment(updated app.listen)

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ mongoose
   .then(() => {
     console.log('✅ MongoDB connected')
 
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`✅ Server running on port ${PORT}`)
     })
   })
